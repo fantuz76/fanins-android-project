e9ff86bcd558480ca8503b3afc7f47921166dd7c
fantuz76@gmail.com <fantuz76@gmail.com@ad937dc0-20de-78df-cf5b-1517efc812e0>
2014-03-07 16:52:59 +0000

    rinomina file per upload su dropbox provato ma non va lettura data base scarica i DB in locale nella cartella DB

--- trunk/FanINS/src/com/fant/fanins/ReadTxtActivity.java
+++ trunk/FanINS/src/com/fant/fanins/ReadTxtActivity.java
@@ -1,8 +1,13 @@
 package com.fant.fanins;
 
+import com.fant.fanins.MyDatabase.DataINStable;
+
 import android.app.ListActivity;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -10,7 +15,11 @@ import android.widget.TextView;
 
 public class ReadTxtActivity extends ListActivity {
 
+	private MyDatabase DBINSlocal, DBINSdownloaded;
+	
 	public static String versionName = "";
+	
+	private SimpleCursorAdapter dataAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +52,108 @@ public class ReadTxtActivity extends ListActivity {
 */	
 		//spinner.setAdapter(adapter1);	// Apply the adapter to the spinner
 
-        
+      /*
         String[] my_string_list = getResources().getStringArray(R.array.Categoria);
         ListView listView = (ListView)findViewById(android.R.id.list);        
         ArrayAdapter<String> arrayAdapter =
                 new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, my_string_list);
         listView.setAdapter(arrayAdapter);
+     */
+
+		// prepara file
+		
+		DBINSlocal = new MyDatabase(
+				getApplicationContext(), 
+				myGlobal.getStorageFantDir().getPath() + java.io.File.separator +  myGlobal.LOCAL_DB_FILENAME);
+				
+		DBINSdownloaded = new MyDatabase(
+				getApplicationContext(), 
+				myGlobal.getStorageDatabaseFantDir().getPath() + java.io.File.separator + myGlobal.LOCAL_DOWNLOADED_DB_FILE);
+		
+		
+	
+		DBINSlocal.open();
+		if (DBINSlocal.fetchProducts().getCount() == 0) {
+			assert true;	// nop
+		} else {
+			Cursor mycursor;
+			mycursor = DBINSlocal.fetchProducts();
+			while ( mycursor.moveToNext() ) {
+
+			    Log.i(myGlobal.TAG, " FANTUZ --> " +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.DATA_OPERAZIONE_KEY) ) +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.TIPO_OPERAZIONE_KEY) ) + 
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.CHI_FA_KEY) ) +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.A_DA_KEY) ) +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.C_PERS_KEY) ) +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.VALORE_KEY) ) +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.CATEGORIA_KEY) ) +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.GENERICA_KEY) ) +
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.DESCRIZIONE_KEY) ) + 
+			    		mycursor.getString( mycursor.getColumnIndex(MyDatabase.DataINStable.NOTE_KEY) )
+			    );
+			
+			    // The desired columns to be bound
+			    String[] columns = new String[] {
+			    		MyDatabase.DataINStable.DATA_OPERAZIONE_KEY,
+			    		MyDatabase.DataINStable.TIPO_OPERAZIONE_KEY,
+			    		MyDatabase.DataINStable.CHI_FA_KEY,
+			    		MyDatabase.DataINStable.A_DA_KEY,
+			    		MyDatabase.DataINStable.C_PERS_KEY,
+			    		MyDatabase.DataINStable.VALORE_KEY,
+			    		MyDatabase.DataINStable.CATEGORIA_KEY,
+			    		MyDatabase.DataINStable.GENERICA_KEY
+			    };
+			    
+			    // the XML defined views which the data will be bound to
+			    int[] to = new int[] { 
+			      	10,
+			      	10,
+			      	20,
+			      	30,
+			      	10,
+			      	10,
+			      	20,
+			      	30,
+
+			    };
+			   
+			    
+			    dataAdapter = new SimpleCursorAdapter(
+			    	    this, R.layout.activity_read_txt, 
+			    	    mycursor, 
+			    	    columns, 
+			    	    to,
+			    	    0);
+			    
+			    ListView listView;
+			    listView = (ListView) findViewById(android.R.id.list);
+			    // Assign adapter to ListView
+			    listView.setAdapter(dataAdapter);
+
+			    
+		        String[] my_string_list = getResources().getStringArray(R.array.Categoria);
+		        listView = (ListView)findViewById(android.R.id.list);        
+		        ArrayAdapter<String> arrayAdapter =
+		                new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, columns);
+		        listView.setAdapter(arrayAdapter);			    
+			}   
+
+		}
+		DBINSlocal.close();
+/*
+        String[] my_string_list = getResources().getStringArray(R.array.Categoria);
+        ListView listView = (ListView)findViewById(android.R.id.list);        
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, my_string_list);
+        listView.setAdapter(arrayAdapter);
+		
+		List<Contact> contact = new ArrayList<Contact>(); 
+		contact=getAllContacts();     
+		ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, contact); 
+		 listContent.setAdapter(adapter);
+        
+        */
         
 		
 	}
